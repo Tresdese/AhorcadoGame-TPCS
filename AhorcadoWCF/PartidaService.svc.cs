3dcfe5085be7379c6d4bc676889618bea141ce3c
tresdese <super_diegobros@hotmail.com>
2026-06-05 22:02:26 -0600

    Implementa servicios WCF delegando consultas a los DAOs

--- AhorcadoWCF/PartidaService.svc.cs
+++ AhorcadoWCF/PartidaService.svc.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using AhorcadoWCF.DAOs;
 
 namespace AhorcadoWCF
 {
@@ -11,8 +12,51 @@ namespace AhorcadoWCF
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione PartidaService.svc o PartidaService.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class PartidaService : IPartidaService
     {
-        public void DoWork()
+        private readonly PartidaDAO partidaDAO = new PartidaDAO();
+
+        public PartidaDTO CrearPartida(int idJugadorCreador)
         {
+            return partidaDAO.Crear(idJugadorCreador);
+        }
+
+        public List<PartidaDTO> ObtenerPartidasDisponibles()
+        {
+            return partidaDAO.ObtenerDisponibles();
+        }
+
+        public bool VerificarDisponibilidadPartida(int idPartida)
+        {
+            return partidaDAO.VerificarDisponibilidad(idPartida);
+        }
+
+        public bool UnirseAPartida(int idPartida, int idJugadorAdivinador)
+        {
+            return partidaDAO.Unirse(idPartida, idJugadorAdivinador);
+        }
+
+        public PartidaDTO ObtenerEstadoPartida(int idPartida)
+        {
+            return partidaDAO.ObtenerEstado(idPartida);
+        }
+
+        public bool CancelarPartida(int idPartida)
+        {
+            return partidaDAO.Cancelar(idPartida);
+        }
+
+        public bool AbandonarPartida(int idPartida, int idJugadorQueAbandona)
+        {
+            return partidaDAO.Abandonar(idPartida, idJugadorQueAbandona);
+        }
+
+        public bool FinalizarPartida(int idPartida, int idGanador, string tipoPuntaje)
+        {
+            return partidaDAO.Finalizar(idPartida, idGanador, tipoPuntaje);
+        }
+
+        public bool ReabrirPartida(int idPartida)
+        {
+            return partidaDAO.Reabrir(idPartida);
         }
     }
 }
--- AhorcadoWCF/UsuarioService.svc.cs
+++ AhorcadoWCF/UsuarioService.svc.cs
@@ -1,9 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using AhorcadoWCF.DAOs;
 
 namespace AhorcadoWCF
 {
@@ -11,8 +12,51 @@ namespace AhorcadoWCF
     // NOTA: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione UsuarioService.svc o UsuarioService.svc.cs en el Explorador de soluciones e inicie la depuración.
     public class UsuarioService : IUsuarioService
     {
-        public void DoWork()
+        private readonly UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+        public bool IniciarSesion(string correo, string contrasena)
         {
+            return usuarioDAO.ValidarCredenciales(correo, contrasena);
+        }
+
+        public bool RegistrarCuenta(UsuarioDTO usuario)
+        {
+            if (usuario == null)
+            {
+                return false;
+            }
+
+            return usuarioDAO.RegistrarCuenta(usuario);
+        }
+
+        public UsuarioDTO ObtenerPerfil(int idUsuario)
+        {
+            return usuarioDAO.ObtenerPorId(idUsuario);
+        }
+
+        public bool ActualizarPerfil(UsuarioDTO usuario)
+        {
+            if (usuario == null)
+            {
+                return false;
+            }
+
+            return usuarioDAO.Actualizar(usuario);
+        }
+
+        public bool CambiarContrasena(int idUsuario, string nuevaContrasena)
+        {
+            return usuarioDAO.CambiarContrasena(idUsuario, nuevaContrasena);
+        }
+
+        public bool VerificarCorreoExistente(string correo)
+        {
+            return usuarioDAO.ExisteCorreo(correo);
+        }
+
+        public bool ValidarSesionActiva(int idUsuario)
+        {
+            throw new NotImplementedException();
         }
     }
 }
