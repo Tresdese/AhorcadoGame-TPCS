143d33060bda2dc7d645c5c8079387c6814d7ad7
Gosty <marianasv628@gmail.com>
2026-06-28 21:18:58 -0600

    Agregar GestorIdioma y fijar español por defecto al iniciar

--- ClienteAhorcado/App.xaml.cs
+++ ClienteAhorcado/App.xaml.cs
@@ -8,7 +8,10 @@ using System.Windows;
 
 namespace ClienteAhorcado
 {
-    public partial class App : Application
-    {
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            GestorIdioma.Aplicar(SesionActual.Idioma);
+        }
     }
 }
---
+++ ClienteAhorcado/GestorIdioma.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClienteAhorcado {
+    internal class GestorIdioma {
+        public static void Aplicar(string idioma) {
+            var cultura = new CultureInfo(idioma);
+            Thread.CurrentThread.CurrentUICulture = cultura;
+            Thread.CurrentThread.CurrentCulture = cultura;
+            CultureInfo.DefaultThreadCurrentUICulture = cultura;
+            CultureInfo.DefaultThreadCurrentCulture = cultura;
+            SesionActual.Idioma = idioma;
+        }
+
+        public static void Cambiar(string idioma) {
+            Aplicar(idioma);
+
+            var actual = Application.Current.Windows
+                .OfType<Window>()
+                .FirstOrDefault(w => w.IsActive) ?? Application.Current.MainWindow;
+
+            if (actual == null) return;
+
+            var nueva = (Window)Activator.CreateInstance(actual.GetType());
+            Application.Current.MainWindow = nueva;
+            nueva.Show();
+            actual.Close();
+        }
+    }
+}
